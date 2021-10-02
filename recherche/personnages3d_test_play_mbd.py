a78cf0f4def647985d9916cfaf4012525ab231d9
mxBossard <mxbossard@gmail.com>
2021-10-02 15:28:40 +0200

    Clean the code.

--- recherche/personnages3d_test_play_mbd.py
+++ recherche/personnages3d_test_play_mbd.py
@@ -346,173 +346,15 @@ class Personnages3D:
         """Unité = mm"""
         self._repo = PersonnagesCoordinatesRepo(12)
 
-        # Distance de rémanence pour attribution des squelettes
-        self.distance = 500
-
         self.json_data = read_json(FICHIER)
-
-        self.whos = [0]*4
 
         # Fenêtre pour la vue du dessus
         cv2.namedWindow('vue du dessus', cv2.WND_PROP_FULLSCREEN)
         self.black = np.zeros((720, 1280, 3), dtype = "uint8")
 
-        # Toutes les datas des personnages dans un dict self.personnages
-        self.personnages = []
-        for i in range(4):
-            self.personnages.append(Personnage())
-        self.skelet_nbr = 0
-        self.new_centers = None
-
-        self.loop = 1
+        self.loop = True
         # Numéro de la frame
-        self.nbr = 0
-
-    def main_frame_test(self, skelet_3D):
-        """skelet_3D sont les squelettes en 2D et 3D"""
-
-        if skelet_3D:
-            # Nombre de squelettes
-            self.skelet_nbr = len(skelet_3D)
-
-            self.who_is_who(skelet_3D)
-            self.apply_to_personnages(skelet_3D)
-            self.draw_all_personnages()
-
-        else:
-            print(f"\nPas de squelette  ------------------------>",
-               f"             Frame --->",
-               f"{self.nbr}")
-
-    def who_is_who(self, skelet_3D):
-
-        # Préliminaire
-        self.update_centers(skelet_3D)
-        print(f"\nNombre de squelette  ------------------------>",
-               f"{self.skelet_nbr}         Frame --->",
-               f"{self.nbr}")
-
-        # Parcours des squelettes pour calculer les distances par rapport
-        # aux centres des personnages
-        # dists[0] = liste des distance entre: squelette 0 et les personnages
-        dists = {}
-        for skel in range(self.skelet_nbr):
-            dists[skel] = []
-            # Recherche des perso proche de ce skelelet
-            for perso in range(4):
-                dist = get_distance(self.new_centers[skel], self.last_centers[perso])
-                if dist > 100000:
-                    dist = 100000
-                dists[skel].append(dist)
-
-        print("distances:", dists)  # {0: [41, 41, 41, 41]}
-
-        # Attibution avec le perso le plus proche du squelette
-        whos, TODO = self.attribution_with_nearest(dists)
-        self.whos = self.default_attribution(whos, TODO)
-
-    def update_centers(self, skelet_3D):
-        """
-        last_centers = liste des centres tirée des centres des personnages
-        self.new_centers = liste des centres des squelettes de la frame
-        """
-
-        self.last_centers = []
-        for i in range(4):
-            self.last_centers.append(self.personnages[i].center)
-
-        self.new_centers = []
-        for i in range(self.skelet_nbr):
-            self.new_centers.append(get_center(skelet_3D[i]))
-
-    def attribution_with_nearest(self, dists):
-        """ Attribution avec le plus près
-        Nombre de squelette  ------------------------> 2
-        distances: {0: [2, 1091, 1557, 100000], 1: [1092, 3, 1415, 100000]}
-        whos: [0, 1, None, None] TODO: 0
-        whos final [0, 1, None, None]
-
-        Nombre de squelette  ------------------------> 2
-        distances: {0: [1091, 2, 1413, 100000], 1: [3, 1096, 1556, 100000]}
-        whos: [1, 0, None, None] TODO: 0
-        whos final [1, 0, None, None]
-        """
-        whos = [None]*4
-        # Nombre de squelette qui reste à attribuer
-        TODO = self.skelet_nbr
-        for i in range(self.skelet_nbr):
-            if i in dists:
-                # Le mini dans la liste
-                mini = min(dists[i])
-                # Position du mini dans la liste
-                index = dists[i].index(mini)
-                if mini < self.distance:
-                    whos[index] = i
-                    TODO -= 1
-
-        return whos, TODO
-
-    def default_attribution(self, whos, TODO):
-        """ Attribution par défaut si pas attribué avant
-
-        3 squelttes
-        whos: [1, None, None, None] --> TODO: 2
-        On doit trouver --> [1, 0, 2, None]
-
-        liste des déjà attribués: done = [1]
-        à attribuer 0 et 2:
-            possible = [0, 2, 3]
-            moins whos
-            liste des numéros à attribuer: dispo = [0, 2]
-        len(dispo) = TODO
-        """
-
-        done = [x for x in whos if x is not None]
-        dispo = [x for x in range(4) if x not in whos]
-
-        print("whos avec nearest:", whos, "TODO:", TODO, "done:", done, "dispo", dispo)
-
-        d = 0
-        while TODO > 0:
-            for i, who in enumerate(whos):
-                if who is None:
-                    whos[i] = dispo[d]
-                    TODO -= 1
-                    d += 1
-                    break
-
-        print("whos final:", whos)
-        return whos
-
-    def apply_to_personnages(self, skelet_3D):
-        """ whos du type [1, 0, None, 2]
-                                1 attribué au perso 0
-                                0 attribué au perso 1 ... etc ...
-        """
-
-        for i in range(4):
-            # Data valide
-            if self.whos[i] is not None:
-                self.personnages[i].who = self.whos[i]
-                self.personnages[i].points_3D = skelet_3D[self.whos[i]]
-                c = get_center(skelet_3D[self.whos[i]])
-                self.personnages[i].center = c
-                self.personnages[i].last_update = self.nbr
-                self.personnages[i].stable += 1
-                if self.personnages[i].stable > 3:
-                    self.personnages[i].stable = 3
-
-            # Pas de data sur cette frame
-            else:
-                self.personnages[i].who = None
-                self.personnages[i].points_3D = None
-                self.personnages[i].center = [100000]*2
-                self.personnages[i].stable -= 1
-
-        # Reset si pas d' attribution pendant 5 frames
-        for i in range(4):
-            if self.nbr - self.personnages[i].last_update > 4:
-                self.personnages[i].reset()
+        self.frame = 0
 
     def draw_all_personnages(self):
         """Dessin des centres des personnages dans la vue de dessus,
@@ -536,8 +378,8 @@ class Personnages3D:
 
         while self.loop:
 
-            if self.nbr < len(self.json_data):
-                skelet_3D = self.json_data[self.nbr]
+            if self.frame < len(self.json_data):
+                skelet_3D = self.json_data[self.frame]
                 #self.main_frame_test(skelet_3D)
                 if skelet_3D:
                     coordinates = []
@@ -550,14 +392,14 @@ class Personnages3D:
                         coordinates.append((x, y, 0))
 
                     #print("DEBUG: all coordinates [%s]" % coordinates)
-                    self._repo.addNewFrameCoordinates(self.nbr + 1, coordinates)
+                    self._repo.addNewFrameCoordinates(self.frame + 1, coordinates)
 
                     self.draw_all_personnages()
             else:
-                self.loop = 0
+                self.loop = False
 
             cv2.imshow('vue du dessus', self.black)
-            self.nbr += 1
+            self.frame += 1
 
             k = cv2.waitKey(76)
             if k == 27:  # Esc
@@ -575,19 +417,6 @@ def read_json(fichier):
         data = None
         print("Fichier inexistant ou impossible à lire:")
     return data
-
-
-def get_distance(p1, p2):
-    """Distance entre les points p1 et p2, dans le plan horizontal,
-    sans prendre en compte le y qui est la verticale.
-    """
-
-    if p1 and p2:
-        if None not in p1 and None not in p2:
-            d = ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
-            return int(d)
-    return 100000
-
 
 def get_center(points_3D):
     """Le centre est le centre de vue du dessus,
